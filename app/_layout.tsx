ef857705fa961ef52858ebd71b9837c6ea9992ef
FacuM <facumo.fm@gmail.com>
2025-03-05 21:53:22 -0300

    Add SEO metadata to RootLayout component
    - Implements Head component to include title and description.
    
    - Enhances the application's SEO by providing relevant page metadata.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
+import Head from 'expo-router/head';
+
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <Head>
+        <title>
+          WPrint 3D - Print from anywhere!
+        </title>
+        <meta name="description" content="Print from anywhere!" />
+      </Head>
+    
+      <Stack screenOptions={{ headerShown: false }} />;
+    </>
+  );
 }
